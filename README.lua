4f799cbcb7da0c9e952ed7fbcdf81c5b0c32a828
asciidude <sickspamemail@gmail.com>
2024-07-12 21:14:53 -0500

    init

---
+++ README.lua
@@ -0,0 +1,21 @@
+# Truck Jobs 🚚
+Truck Jobs is a plugin that allows players to take on the open road via truck driving jobs where they can complete deliveries and earn payouts.
+
+**Note:** Payouts are there for roleplay aspect. I may work on some sort of standalone banking framework if this gets enough traction and enable this to work with that.
+
+## Features
+- :eyes: **Job Dashboard:** A user-friendly interface to view and select available jobs.
+- :construction_worker_man: **Dynamic Job Assignments:** Jobs are dynamically assigned based off the selected truck.
+- :white_check_mark: **Completion Markers:** Ground markers at the drop-off locations to guide players.
+- :money_mouth_face: **Payouts:** Earn (fake) money at the end of every job.
+- :mechanical_arm: **Job State Management:** Robust job state management to prevent premature job endings.
+- :white_circle: **Blip Integration:** Blips on the map for vehicles and delivery points.
+- :gear: **Extremely Configurable:** Lots of configuration options to ensure this plugin is exactly how you want it!
+- :oncoming_automobile: **Automatic Vehicle Cleanup:** Cleans up trucks when the job ending is triggered.
+- :person_juggling: **Optimized for Multiple Players:** This plugin has been optimized for multiple players to use it at once.
+
+## Installation
+- Drag and drop `truckjobs` into your `/resources` folder
+- Configure `TruckList.lua` & `config.lua` to your liking
+- Add `ensure truckjobs` to your `server.cfg`
+- Start or restart your server and enjoy! :smiley: 
